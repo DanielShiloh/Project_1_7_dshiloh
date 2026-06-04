2821ea2a26cb8ee91ab88eedd4a4edfa22bc2444
Daniel Shiloh <dshiloh@cscc.edu>
2026-06-03 23:11:31 -0400

    Expanded logic into code

--- project1.py
+++ project1.py
@@ -1,3 +1,6 @@
+#constants
+FORM_WIDTH = 40
+
 #introductory message
 intro = "Thank you for choosing Daniel's Dog Clinic."
 intro += "\nPlease fill out this form to request an appointment."
@@ -22,28 +25,39 @@ else:
 
         #create dog's profile
         current_dog = {
-            'name': input("What is your dog's name? ").lower(),
+            'name': input("What is your dog's name? ").title(),
             'sex': input("What is your dog's sex? ").lower(),
             'breed': input("What is your dog's breed? ").lower(),
             'age': input("What is your dog's age in years? "),
             'history': [],
         }
         
-        #add medical history
+        #add medical history list
         print(f"Please answer a few questions about {current_dog['name']}'s medical history.")
         has_dx = input("Do you have any previous diagnoses to report [y/n]? ")
         while has_dx.lower() == "y":
             dx_name = input("What was the diagnosis? ")
             current_dog['history'].append(dx_name)
-            has_dx = input("Do you have another diagnoses to report [y/n]? ")
+            has_dx = input("Do you have another diagnosis to report [y/n]? ")
   
         #add this dog's dictionary to the owner's list of dogs
         dog_db.append(current_dog)
 
-        ######done adding things to this dict
+        #print registration form
+        print(f"Please bring {current_dog['name']}'s completed form to your visit.")
+        print("\n " + "_" * FORM_WIDTH)
+        print(f'|{"Daniel's Dog Clinic":^{FORM_WIDTH}}|')
+        print(f"|{f' Owner: {human_first_name} {human_last_name}':<{FORM_WIDTH}}|")
+        #######etc
 
-        ######print lil form with the data
-
-    ######would you like to register another pet
-
-    #####great we wont be in touch
+        #register another pet
+        repeat = input("Would you like to register another dog [y/n]? ")
+        if repeat.lower() != "y":
+            break
+    
+    #outro message
+    all_dogs_names = ""
+    for dog in dog_db:
+        all_dogs_names += " " + dog['name']
+    print(f"\nThank you for registering{all_dogs_names}.")
+    print("We'll totally for sure be in touch.\n")
