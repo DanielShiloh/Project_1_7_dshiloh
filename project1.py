1e7fb9ca85f532cf629c49b679e8a8b3a3646eb3
Daniel Shiloh <dshiloh@cscc.edu>
2026-06-03 23:37:00 -0400

    Finished print registration form

--- project1.py
+++ project1.py
@@ -43,21 +43,47 @@ else:
         #add this dog's dictionary to the owner's list of dogs
         dog_db.append(current_dog)
 
+
+
         #print registration form
+
+        blank_line = "|" + " " * FORM_WIDTH + "|"
+
         print(f"Please bring {current_dog['name']}'s completed form to your visit.")
-        print("\n " + "_" * FORM_WIDTH)
+
+        print("\n " + "_" * FORM_WIDTH) #upper bound of form
+        
+        print(blank_line)
+       
         print(f'|{"Daniel's Dog Clinic":^{FORM_WIDTH}}|')
+        
+        print(blank_line)
+       
         print(f"|{f' Owner: {human_first_name} {human_last_name}':<{FORM_WIDTH}}|")
-        #######etc
+        print(f"|{f' Dog: {current_dog['name']}, {current_dog['age']} y/o {current_dog['sex']} {current_dog['breed']}':<{FORM_WIDTH}}|")
+        
+        print(blank_line)
+       
+        print(f"|{' Medical History:':<{FORM_WIDTH}}|")
+
+        if current_dog['history']:
+            for item in current_dog['history']:
+                print(f"|{f'     {item}':<{FORM_WIDTH}}|")
+        else: #no history was reported
+            print(f"|{'     none':<{FORM_WIDTH}}|")
+        
+        print(blank_line)
+        
+        print(" " + "-" * FORM_WIDTH) #lower bound of form
 
         #register another pet
-        repeat = input("Would you like to register another dog [y/n]? ")
+        repeat = input("\nWould you like to register another dog [y/n]? ")
         if repeat.lower() != "y":
             break
     
     #outro message
     all_dogs_names = ""
     for dog in dog_db:
-        all_dogs_names += " " + dog['name']
-    print(f"\nThank you for registering{all_dogs_names}.")
+        all_dogs_names += dog['name'] + ", "
+    print(f"\nThank you for registering {all_dogs_names[:-2]}.")
     print("We'll totally for sure be in touch.\n")
