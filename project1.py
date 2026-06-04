f9ffd50d43767be62547edc49fecda7ef7acd2fd
Daniel Shiloh <dshiloh@cscc.edu>
2026-06-03 22:32:29 -0400

    Added list of dogs so owner can have multiple dogs

--- project1.py
+++ project1.py
@@ -1,34 +1,49 @@
+#introductory message
 intro = "Thank you for choosing Daniel's Dog Clinic."
 intro += "\nPlease fill out this form to request an appointment."
 print(intro)
 
+#confirm that this is a dog
 species = input("What species is your pet? ")
 if species.lower() != "dog":
     print(f"We don't see {species}s here, just dogs.  Try MedVet.")
 else:
     print("Great!  Let's get some information on you and your dog.")
+    
+    #get owner name
+    human_first_name = input("What is your first name? ").title()
+    human_last_name = input("What is your last name? ").title()
 
-######first time only - get owner name
+    #initialize list of dogs
+    dog_db = []
 
-while species.lower() == "dog":
-    #######make this into dictionary, humanNameDogName = name:name, sex:sex, breed:breed...
-    name = input("What is your dog's name? ")
-    sex = input("What is your dog's sex? ")
-    breed = input("What is your dog's breed? ")
-    age = input("What is your dog's age in years? ")
+    #register a dog
+    while True:
 
-    print(f"Please answer a few questions about {name} the {age} y/o {sex} {breed}'s medical history.")
+        #create dog's profile
+        current_dog = {
+            'name': input("What is your dog's name? ").lower(),
+            'sex': input("What is your dog's sex? ").lower(),
+            'breed': input("What is your dog's breed? ").lower(),
+            'age': input("What is your dog's age in years? "),
+            'history': [],
+        }
+        
+        #add medical history
+        print(f"Please answer a few questions about {current_dog['name']}'s medical history.")
+        has_dx = input("Do you have any previous diagnoses to report [y/n]? ")
+        while has_dx.lower() == "y":
+            dx_name = input("What was the diagnosis? ")
+            current_dog['history'].append(dx_name)
+            has_dx = input("Do you have another diagnoses to report [y/n]? ")
+  
+        #add this dog's dictionary to the owner's list of dogs
+        dog_db.append(current_dog)
 
-    has_dx = input("Do you have any previous diagnoses to report [y/n]? ")
-    while has_dx.lower() == "y":
-        break
-        #####get dx and date
-        #####repeat question
+        ######done adding things to this dict
 
-    ######done adding things to this dict
+        ######print lil form with the data
 
-    ######print lil form with the data
+    ######would you like to register another pet
 
-######would you like to register another pet
-
-#####great we wont be in touch
+    #####great we wont be in touch
