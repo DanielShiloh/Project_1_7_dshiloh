bc324a262d1f62c936d2756d31c394c06fb98717
Daniel Shiloh <dshiloh@cscc.edu>
2026-06-17 23:01:27 -0400

    Moved short actions to functions

--- project1.py
+++ project1.py
@@ -2,7 +2,7 @@
 Dog Registration Form-Filler
 Daniel Shiloh
 Pre-fill a registration form with owner and dog information
-June 11, 2026
+June 17, 2026
 """
 
 FORM_WIDTH = 40
@@ -10,20 +10,69 @@ blank_line = "|" + " " * FORM_WIDTH + "|"
 dog_db = []
 all_dogs_names = ""
 
-intro = "Thank you for choosing Daniel's Dog Clinic."
-intro += "\nPlease fill out this form to request an appointment."
-print(intro)
+def print_intro():
+    """Print intro message to user"""
+    intro = "Thank you for choosing Daniel's Dog Clinic."
+    intro += "\nPlease fill out this form to request an appointment."
+    print(intro)
+
+def decline_pet():
+    """Decline non-dog pet, give chance to enter another pet"""
+    print(f"We don't see {species}s here, just dogs.  Try MedVet.")
+    species = input("Think carefully... what species is your pet? ")
+
+def get_human_info():
+    """Get human's name"""
+    human_first_name = input("What is your first name? ").title()
+    human_last_name = input("What is your last name? ").title()
+    return human_first_name, human_last_name
+
+def print_registration_form(first, last, dog):
+    """Print human and dog info to screen
+    Args:
+        first (str): human first name
+        last (str): human last name
+        dog (dict): name, age, sex, breed, medical history (list)
+    """
+
+    print("\n " + "_" * FORM_WIDTH)
+    
+    print(blank_line)
+    
+    print(f'|{"Daniel's Dog Clinic":^{FORM_WIDTH}}|')
+    
+    print(blank_line)
+    
+    print(f"|{f' Owner: {first} {last}':<{FORM_WIDTH}}|")
+    print(f"|{f' Dog: {dog['name']}, {dog['age']} y/o {dog['sex']} {dog['breed']}':<{FORM_WIDTH}}|")
+    
+    print(blank_line)
+    
+    print(f"|{' Medical History:':<{FORM_WIDTH}}|")
+
+    if dog['history']:
+        for item in dog['history']:
+            print(f"|{f'     {item}':<{FORM_WIDTH}}|")
+    else:
+        print(f"|{'     none':<{FORM_WIDTH}}|")
+    
+    print(blank_line)
+    
+    print(" " + "-" * FORM_WIDTH)
+
+
+
+
+print_intro()
 
 species = input("What species is your pet? ")
 
 while species != "dog":
-    print(f"We don't see {species}s here, just dogs.  Try MedVet.")
-    species = input("Think carefully... what species is your pet? ")
+    decline_pet()
 
 print("Great!  Let's get some information on you and your dog.")
     
-human_first_name = input("What is your first name? ").title()
-human_last_name = input("What is your last name? ").title()
+first, last = get_human_info()
 
 while True: #register infinite dogs
 
@@ -46,32 +95,7 @@ while True: #register infinite dogs
 
     print(f"Please bring {current_dog['name']}'s completed form to your visit.")
     
-    #print registration form --start
-    print("\n " + "_" * FORM_WIDTH)
-    
-    print(blank_line)
-    
-    print(f'|{"Daniel's Dog Clinic":^{FORM_WIDTH}}|')
-    
-    print(blank_line)
-    
-    print(f"|{f' Owner: {human_first_name} {human_last_name}':<{FORM_WIDTH}}|")
-    print(f"|{f' Dog: {current_dog['name']}, {current_dog['age']} y/o {current_dog['sex']} {current_dog['breed']}':<{FORM_WIDTH}}|")
-    
-    print(blank_line)
-    
-    print(f"|{' Medical History:':<{FORM_WIDTH}}|")
-
-    if current_dog['history']:
-        for item in current_dog['history']:
-            print(f"|{f'     {item}':<{FORM_WIDTH}}|")
-    else:
-        print(f"|{'     none':<{FORM_WIDTH}}|")
-    
-    print(blank_line)
-    
-    print(" " + "-" * FORM_WIDTH)
-    #print registration form --end
+    print_registration_form(first, last, current_dog)
 
     repeat = input("\nWould you like to register another dog [y/n]? ")
     if repeat.lower() != "y":
